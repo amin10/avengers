bfb4fe9f93df8c44a4887b1a011bc494d1ccc676
amin_manna <amin10@meet.mit.edu>
2014-08-07 14:33:02 +0300

    Awesome Grid is WORKINGgit add .

--- js/avengers.js
+++ js/avengers.js
@@ -1,23 +1,26 @@
+var box_side;
 
 $(function() {
-    for(var i = 1; i < 4; i++){
-        $("#grid-container").append(make_row(i));
+    for(var i = 0; i < 3; i++){
+        var row = make_row(i);
+        for(var j = 0; j < 3; j++){
+            row.append(make_div(j))
+        }
+        $("#grid-container").append(row);
     }
-    var curRow;
-    for(var i = 0; i < 9; i++){
-        curRow = (i/3);
-        $('#grid-container:nth-child(" + curRow + ")').append(make_div(i));
-    }    
+    box_side = $(".boxes").css('height');
+    $("#grid-container").css('width',parseInt(box_side)*5);
 });
 
 function make_row(x) {
     var newRow = $("<div></div>");
     newRow.attr('id', 'row' + String(x));
     newRow.addClass('rows');
+    return newRow;
 }
 
 function make_div(x){
-    var newBox = $("<div></div>");
+    var newBox = $("<span></span>");
     newBox.attr('id', 'box'+String(x));
     newBox.addClass('boxes').addClass('faded')
     newBox.hover(function(){
@@ -26,8 +29,9 @@ function make_div(x){
         //$(".boxes:not(.active)").addClass('faded'); 
         $(this).animate({width:'+=400px', height:'+=400px'}, 1500)
     },function(){
+        $(this).stop()
         $(this).removeClass('active').addClass('faded');
-        $(this).animate({width:'-=400px', height:'-=400px'}, 1500)
+        $(this).animate({width: box_side, height:box_side}, 0)
     });
     return newBox;
 }
