62fe3b7ca21f4716ea34368c6b90453444055b0b
amin_manna <amin10@meet.mit.edu>
2014-08-07 13:17:25 +0300

    divs clear

--- js/avengers.js
+++ js/avengers.js
@@ -13,10 +13,10 @@ function make_div(x){
         //$(".active").removeClass('active');
         $(this).removeClass('faded').addClass('active');
         //$(".boxes:not(.active)").addClass('faded'); 
-        $(this).animate({width:'+=400px', height:'+=400px'}, 200)
+        $(this).animate({width:'+=400px', height:'+=400px'}, 1500)
     },function(){
         $(this).removeClass('active').addClass('faded');
-        $(this).animate({width:'-=400px', height:'-=400px'}, 200)
+        $(this).animate({width:'-=400px', height:'-=400px'}, 1500)
     });
     return newBox;
 }
