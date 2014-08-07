387040d99b7ace95ec7d8d8111c9c1f5d45a58d6
amin_manna <amin10@meet.mit.edu>
2014-08-07 12:06:55 +0300

    JS siblings using :not

--- js/avengers.js
+++ js/avengers.js
@@ -8,10 +8,11 @@ function make_div(x){
     var newBox = $("<div></div>");
     newBox.attr('id', 'box'+String(x));
     newBox.addClass('boxes');
-    newBox.css('height','50px').css('width','50px').css('background-color', 'red');
     newBox.hover(function(){
-        $(this).css("height","200%").css("width", "200%");
-        $(this).siblings().addClass('faded') 
+        $(this).css("height","200%").css("width", "200%").removeClass('faded');
+        $(".active").removeClass('.active');
+        $(this).addClass('active');
+        $(".boxes:not(.active)").addClass('faded'); 
         $(this).fadeIn()
     });
     return newBox;
